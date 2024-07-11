a0992546a63a50071baa3c4546d8f68adad28d56
Rohan Naveed <rohan.naveed626899@gmail.com>
2024-07-12 01:40:49 +0500

    Made Contact Section And Integrated SMTP Email Server Except Message To Be Sent

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,35 @@ var typed = new Typed(".typing", {
   backSpeed: 175,
   loop: true,
 });
+
+const form = document.querySelector("form");
+const senderFullName = document.getElementById("name").textContent;
+const senderPhone = document.getElementById("phone").textContent;
+const senderEmail = document.getElementById("email").textContent;
+const senderMessage = document.getElementById("message").textContent;
+
+console.log(form);
+
+let msgString = `Sender's Full Name : ${senderFullName} <br>
+                  Sender's Phone : ${senderPhone} <br>
+                  Sender's Email : ${senderEmail} <br>
+                  Sender's Message : ${senderMessage}`;
+
+console.log(msgString);
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "rohan.naveed626899@gmail.com",
+    Password: "2E9E5D303028F0A918E035756F9BC4184022",
+    To: "rohan.naveed626899@gmail.com",
+    From: "rohan.naveed626899@gmail.com",
+    Subject: "Message From Personal Portfolio",
+    Body: msgString,
+  }).then((message) => alert(message));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  sendEmail();
+});
